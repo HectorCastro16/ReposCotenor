f9e13054ca8c8fe8df6523ef2693a381448bf468
HectorCastro04 <hectorcs464@gmail.com.pe>
2016-04-10 08:00:46 -0500

    Conexion

---
+++ SysCotenor/CapaAccesoDatos/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaAccesoDatos
+{
+    public class Conexion
+    {
+        #region Singleton
+        private static readonly Conexion _Instancia = new Conexion();
+        public static Conexion Instancia
+        {
+            get
+            {
+                return Conexion._Instancia;
+            }
+        }
+        #endregion Singleton
+
+        #region metodos
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=.; Initial Catalog=BD_Cotenor;User ID=sa; Password=123456";
+            return cn;
+        }
+        #endregion metodos
+
+    }
+}
