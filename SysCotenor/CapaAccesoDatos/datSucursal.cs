db78ab69b9f3eb5b0608629c166e3a2fa58bfb07
HectorCastro04 <hectorcs464@gmail.com.pe>
2016-04-26 21:48:26 -0500

    Act

---
+++ SysCotenor/CapaAccesoDatos/datSucursal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using CapaEntidades;
+
+namespace CapaAccesoDatos
+{
+    public class datSucursal
+    {
+
+
+
+    }
+}
